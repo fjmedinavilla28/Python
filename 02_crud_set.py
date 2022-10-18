dae8d41074dc71bcbd34067c84e26fd717451a0d
Francisco Medina <115595230+fjmedinavilla28@users.noreply.github.com>
2022-10-18 21:44:02 +0000

    incluyendo archivo con métodos para conjuntos.

--- 02_crud_set.py
+++ 02_crud_set.py
@@ -1,2 +1,34 @@
 set_countries = {'col', 'mex', 'bol'}
+size = len(set_countries)
+print(size)
+
+print('col' in set_countries)
+print('pe' in set_countries)
+
+#add
+
+set_countries.add('pe')
 print(set_countries)
+
+#update
+
+set_countries.update({'ar','ecua','pe'})
+print(set_countries)
+
+#remove
+set_countries.remove('ar')
+print(set_countries)
+
+#no genera error porque no existe arg, solo dice que no está.
+
+set_countries.discard('arg')
+print(set_countries)
+
+set_countries.add('arg')
+print(set_countries)
+
+#borra absolutamente todo, no deja nada, un borrado de bajo nivel
+
+set_countries.clear()
+print(set_countries)
+print(len(set_countries))
